c20f2990974f9ec2600208d9f39ae9c2e14ff8f0
Dusan Rajcevic <dusanrajcevic@users.noreply.github.com>
2026-02-14 21:46:19 +0100

    feat: add NullHandler

---
+++ src/Handlers/NullHandler.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Vista\Logger\Handlers;
+
+use Vista\Logger\Contracts\HandlerInterface;
+use Vista\Logger\LogRecord;
+
+/**
+ * Handler that discards all log records.
+ */
+final class NullHandler implements HandlerInterface
+{
+    public function handle(LogRecord $record): void
+    {
+        // Do nothing
+    }
+}
